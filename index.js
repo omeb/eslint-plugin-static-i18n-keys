ee2cd4545eb138e6150d73ef653ab24a20c381ac
omeb <omerb@wix.com>
2023-08-08 11:54:06 +0300

    chore(validator): initial setup

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+    meta: {
+      type: 'problem',
+      docs: {
+        description: 'Enforce valid usage of the "t" function',
+        category: 'Best Practices',
+        recommended: true,
+      },
+      fixable: null,
+      schema: [], // No options to configure
+    },
+    create: function (context) {
+      return {
+        CallExpression(node) {
+          if (
+            node.callee.name === 't' &&
+            node.arguments.length === 1 &&
+            node.arguments[0].type === 'Literal' &&
+            typeof node.arguments[0].value === 'string'
+          ) {
+            return; // Valid usage
+          }
+  
+          context.report({
+            node: node,
+            message: 'Function "t" should only be called with a single static string value.',
+          });
+        },
+      };
+    },
+  };
